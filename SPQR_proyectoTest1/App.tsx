ce4aaa30ba89d7ab4102ccae65dee07e873a00d9
AnthonioFuentes09 <97061981+AnthonioFuentes09@users.noreply.github.com>
2026-01-27 20:55:02 -0600

    2026-01-27 | CustomButtom commit

--- SPQR_proyectoTest1/App.tsx
+++ SPQR_proyectoTest1/App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomButton from './src/components/customButtom';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+       <CustomButton />
     </View>
   );
 }
