8da1abaf5fdaba231f5c5d7c00bfc94566d47bca
AnthonioFuentes09 <97061981+AnthonioFuentes09@users.noreply.github.com>
2026-01-28 20:50:57 -0600

    2026-01-28 | Customazing buttons

--- SPQR_proyectoTest1/App.tsx
+++ SPQR_proyectoTest1/App.tsx
@@ -1,16 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import CustomButton from './src/components/customButtom';
+import CustomButton from './src/components/CustomButtom';
+import { useState } from 'react';
+
 
 export default function App() {
+  const [newTitle, setNewTitle] = useState("Initial Title");
+  
+  const handleOnClick = () => {
+    setNewTitle("Button clicked!");
+    alert("Button was clicked!");
+  }
+
+  const unHandleOnClick = () => {
+    setNewTitle("Button unclicked!");
+    alert("Button was unclicked!");
+  }
+
+  const resetHandleOnClick = () => {
+    setNewTitle("Initial Title");
+    alert("Button was reset!");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start workin2g on your app!</Text>
       <StatusBar style="auto" />
-       <CustomButton />
+      <View style={buttonRowStyle.buttonRow}>
+        <CustomButton  title={"Click me!"} color="blue" onClick={ handleOnClick} />
+        <CustomButton  title={"unClick me!"} color="red" onClick={ unHandleOnClick} />
+        <CustomButton  title={"Reset me!"} color="orange" onClick={ resetHandleOnClick} />
+      </View>
+      <Text>{newTitle}</Text>
     </View>
   );
 }
+
+const buttonRowStyle = StyleSheet.create({
+  buttonRow: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 20,
+    gap: 10
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
--- SPQR_proyectoTest1/src/components/CustomButtom.tsx
+++ SPQR_proyectoTest1/src/components/CustomButtom.tsx
@@ -1,10 +1,38 @@
-import { View, Text } from "react-native";
 
-export default function CustomButton (){
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+interface CustomButtonProps {
+    title: string;
+    color: ButtonColor;
+    onClick: () => void;
+}
+
+type ButtonColor = 'blue' | 'red' | 'green' | 'orange';
+
+export default function CustomButton({title, color, onClick} : CustomButtonProps){
+    
     return(
         <View>
-            <Text>Soy un Boton</Text>
+            <TouchableOpacity 
+                onPress={onClick} 
+                style={[styles.button, { backgroundColor: color }]}>
+            <Text style={styles.text}>{title}</Text>
+            </TouchableOpacity>
         </View>
     );
-}
+
+    
+};
+
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 15,
+    borderRadius: 5,
+    marginHorizontal: 5
+  },
+  text: {
+    color: "white",
+    fontSize: 20
+  }
+});
